6a2d7675a188e90720c1f9f5d66390d6c16acc4a
Ashish Lavale <ashishblavale5@gmail.com>
2025-05-20 05:40:39 +0530

    Roboshop-Ansible

--- main.tf
+++ main.tf
@@ -31,4 +31,4 @@ module "databases" {
 #   zone_name                  = var.zone_name
 #   network_security_group_id  = var.network_security_group_id
 #   dns_record_rg_name         = var.dns_record_rg_name
-}
+# }
